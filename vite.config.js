72861435e54349574de5d2438d54984af0d0f87d
brynglbg <bryangil0428@gmail.com>
2026-06-08 15:43:42 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/prsys/",
   plugins: [
     react(),
     tailwindcss()
@@ -12,4 +11,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: "/prsys/",
 })
